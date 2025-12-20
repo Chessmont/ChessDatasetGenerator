7a8777f593fa53ecc2efbe8e2043a7945c3e70b8
Djahmo <clayton.girard@icloud.com>
2025-12-20 16:08:11 +0100

    sd

--- src/regen.js
+++ src/regen.js
@@ -11,7 +11,7 @@ const __dirname = dirname(__filename)
 
 class TSVGenerator {
   constructor() {
-    this.pgnFile = './scripts/data/chessmont.pgn'
+    this.pgnFile = './src/output/chessmont.pgn'
     this.inputFensFile = './src/output/fens-all.tsv'
     this.outputPgiFile = './src/output/chesscom-all-pgi-v2.tsv'
     this.outputFensFile = './src/output/fens-all-v2.tsv'
@@ -106,7 +106,7 @@ class TSVGenerator {
 
   async processPgiBatch(pgnLines, batchId) {
     const result = await this.pgiWorkerPool.execute({ pgnLines, batchId })
-    
+
     for (const pos of result.positions) {
       this.pgiStream.write(`${pos.hashFen}\t${pos.fen}\t${pos.gameId}\t${pos.whiteElo}\t${pos.official}\t${pos.date}\n`)
     }
@@ -175,7 +175,7 @@ class TSVGenerator {
 
   async processFensBatch(lines, batchId, outputStream) {
     const result = await this.fensWorkerPool.execute({ lines, batchId })
-    
+
     for (const line of result.lines) {
       outputStream.write(line + '\n')
     }
